c70a31ece8fa3f3edbf6d9310679bf33c3c383a0
Wisnu <wisnudamar001@gmail.com>
2016-11-26 11:40:14 +0700

    Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl309192939/ensiklopediahewan/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl309192939.ensiklopediahewan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
